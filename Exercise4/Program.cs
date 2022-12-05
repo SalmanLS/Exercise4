8c411ef55ededc28ae19f4c30123f2c083ebabce
SalmanLS <salmanluthfi456@gmail.com>
2022-12-05 15:49:13 +0700

    create function to insert how many element in the array

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -5,11 +5,22 @@ namespace Execise
     class StackUsingArray
     {
         int[] luthfi = new int[25];
-        int i = 0;
+        int i;
 
         public void Read()
         {
-           
+            while (true)
+            {
+                Console.WriteLine("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                i = Int32.Parse(s);
+                if (i <= 25)
+                    break;
+                else
+                    Console.WriteLine("\n Maksimal elemen adalah 25. \n");
+            }
+            
         }
     }
+    
 }
